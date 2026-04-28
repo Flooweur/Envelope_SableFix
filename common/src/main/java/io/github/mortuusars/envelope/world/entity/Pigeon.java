eb2a7c248b68c999096d6173286c3e68ac379489
mortuusars <mortuusars@gmail.com>
2026-04-28 07:47:23 +0300

    Fixed sitting pigeon wing position

--- common/src/main/java/io/github/mortuusars/envelope/world/entity/Pigeon.java
+++ common/src/main/java/io/github/mortuusars/envelope/world/entity/Pigeon.java
@@ -431,7 +431,7 @@ public class Pigeon extends Animal implements VariantHolder<PigeonVariant>, Flyi
 
     @Override
     public boolean isFlying() {
-        return !this.onGround();
+        return !this.onGround() && !isPassenger();
     }
 
     @Override
